bc38f429c59c1481e09fc41f6ffa06a9a5e01589
yuchen <sprball@icloud.com>
2025-12-04 14:38:50 +0800

    Add: Encrypted passwd with SHA256

--- SmartJournalProject/src/Journal/User.java
+++ SmartJournalProject/src/Journal/User.java
@@ -5,6 +5,9 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class User {
@@ -51,7 +54,8 @@ public class User {
                 System.out.print("Enter password: ");
                 password = input.nextLine();
             }
-            outputStream.println(password);
+            String encryptedPasswd = getSHA256(password);
+            outputStream.println(encryptedPasswd);
         } 
         catch (IOException e) {
             System.out.println("Problem with file!!"); 
@@ -71,6 +75,7 @@ public class User {
             email = input.nextLine();
             System.out.print("Enter password: ");
             password = input.nextLine();
+            String encryptedPasswd = getSHA256(password);
             int lineNumber = 0;
             // Check availablity
             while (inputStream.hasNextLine()) {
@@ -80,7 +85,7 @@ public class User {
                     if (currentLine.equals(email)) {
                         // Get displayName and check password
                         displayName = inputStream.nextLine();
-                        if (password.equals(inputStream.nextLine())) {
+                        if (encryptedPasswd.equals(inputStream.nextLine())) {
                             clearScreen();
                             System.out.println("Login successful!");
                             return true;
@@ -112,6 +117,26 @@ public class User {
             return true;
         }
         else return false;
+    }
+
+    public static String getSHA256(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256"); 
+            byte[] hash = md.digest(input.concat("MaybeAddSomeSalt").getBytes(StandardCharsets.UTF_8));
+            String hexString = "";
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                // Pad with a leading zero
+                if (hex.length() == 1) {
+                    hex = "0" + hex;
+                }
+                hexString += hex;
+            }
+            return hexString;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return input;
+        }
     }
 
     private static void clearScreen() {
